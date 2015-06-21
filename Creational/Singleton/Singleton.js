666fa80dc3c638fab600afb82bfafc96112d3ed3
davex69 <avex6996@gmail.com>
2015-06-21 23:25:17 +0700

    Add Singleton

---
+++ Creational/Singleton/Singleton.js
@@ -0,0 +1,42 @@
+/**
+ * JavaScript Singleton implementation
+ */
+define('Singleton', [], function () {
+  var Singleton = (function () {
+
+    // Singleton instance
+    var _instance;
+
+    function _create() {
+      // private property
+      var _bornTime = Date.now();
+
+      // private method
+      function _getBornTime() {
+        return _bornTime;
+      }
+
+      return {
+        // public property
+        name: 'SuperSingleton',
+
+        // public method
+        getInfo: function() {
+          console.log('I born ' + _getBornTime() + ' seconds ago');
+        }
+      };
+    }
+
+    return {
+      getInstance: function () {
+        if (!_instance) {
+          _instance = _create();
+        }
+
+        return _instance;
+      }
+    }
+  })();
+
+  return Singleton;
+});
